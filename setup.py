d1e413687c27dce15386eeb6bd6dcc8484d17267
Walter Doekes <walter+github@wjd.nu>
2013-02-18 15:32:47 +0100

    Add setup.

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='pysigset',
+    version='0.1',
+    description='Signal blocking under Linux',
+    long_description=
+        ('Wrappers around ctypes access to sigprocmask(2) and friends.'),
+    author='Walter Doekes',
+    author_email='wjdoekes@osso.nl',
+    url='https://github.com/ossobv/pysigset',
+    license='GPLv3+',
+    platforms=('linux',),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
+
+# vim: set ts=8 sw=4 sts=4 et ai tw=79:
